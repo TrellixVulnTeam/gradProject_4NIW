d434b757c26da2ad91eb6a7e026ed0eec67afbef
AndresPonciano <andres.ponciano123@gmail.com>
2021-03-26 20:55:15 -0400

    restructuring project and making new database tables

--- server/fetchData.py
+++ server/fetchData.py
@@ -10,9 +10,13 @@ def get_topics():
         print('topic is: ', topic)
 
         search_query = scholarly.search_keyword(topic)
+        # search_query = scholarly.search_pubs(topic)
         for i in range(1):
             print('--------------------------HEEELLOOOOOOOOOOOOOOOOOOOOOOOOOOO--------------------------------------')
-            scholarly.pprint(next(search_query))
+            try:
+                scholarly.pprint(next(search_query))
+            except StopIteration:
+                print('NO KEYWORD FOR: ', topic)
 
 def create_connection(db_file):
     """ create a database connection to the SQLite database
@@ -27,6 +31,19 @@ def create_connection(db_file):
         print(e)
 
     return conn
+
+def create_profile(conn, profile):
+    """
+    create a new profile
+    """
+    sql = ''' INSERT INTO Profile(name, scholar_id, url_picture, interests)
+                VALUES(?,?,?,?) '''
+
+    cur = conn.cursor()
+    cur.execute(sql, profile)
+    conn.commit()
+    return cur.lastrowid
+
 
 def create_user(conn, user):
     """
@@ -50,11 +67,18 @@ def main():
     print(conn)
 
     with conn:
-        user = ('sundiata', 'NY', '2015-01-01');
+        user = ('assata', 'NY', '2015-02-01');
         user_id = create_user(conn, user)
+
+        profile = ('aname222', '124das', 'url1', [
+            {'name': 'Jess'},
+            {'name': '12'},
+        ]
+        )
+        profile_id = create_profile(conn, profile)
 
 # get_topics()
 
 if __name__ == '__main__':
-    main()
+    # main()
     get_topics()
